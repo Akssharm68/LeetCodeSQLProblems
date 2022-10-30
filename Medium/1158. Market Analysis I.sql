035550fb24aa2f7ae9a643bbae19ac7a945f300e
Akshat Sharma <asharm68@stevens.edu>
2022-10-30 19:20:05 -0400

    Create 1158. Market Analysis I.sql
    1158. Market Analysis I
    https://leetcode.com/problems/market-analysis-i/

---
+++ Medium/1158. Market Analysis I.sql
@@ -0,0 +1,9 @@
+SELECT u.user_id AS buyer_id,
+u.join_date,
+COUNT(o.order_id) AS orders_in_2019
+FROM Users u
+LEFT JOIN (
+    SELECT * FROM Orders
+    WHERE YEAR(order_date)=2019
+) o ON (u.user_id=o.buyer_id)
+GROUP BY u.user_id;
