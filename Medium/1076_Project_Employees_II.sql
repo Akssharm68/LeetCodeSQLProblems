63447aea023bdee81af82f76a1bbc707bc9c78bf
Akshat Sharma <asharm68@stevens.edu>
2023-03-04 17:40:17 -0500

    Create 1076_Project_Employees_II.sql
    https://leetcode.com/problems/project-employees-ii/description/

---
+++ Medium/1076_Project_Employees_II.sql
@@ -0,0 +1,11 @@
+SELECT project_id
+FROM project
+GROUP BY project_id
+HAVING COUNT(employee_id) =
+(
+    SELECT count(employee_id)
+    FROM project
+    GROUP BY project_id
+    ORDER BY count(employee_id) desc
+    LIMIT 1
+)
